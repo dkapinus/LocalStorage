4ff411ec0ecfd669794b1725ec59c7b266860592
dkapinus <dimonio@tut.by>
2023-02-10 17:57:42 +0300

    add useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {LocalStorage} from "./LocalStorage";
 
@@ -7,42 +7,36 @@ function App() {
 
     const [value,setValue]=useState(0)
 
+    useEffect(()=>{
+            let GetItem=localStorage.getItem('curentValue')
+            if(GetItem){const newValue =JSON.parse(GetItem)
+                setValue(newValue)}
+        },[]
+    )
+
+    useEffect(()=>{
+        localStorage.setItem('curentValue',JSON.stringify(value))
+        },[value]
+
+    )
+
+
+
 const onClickHandler = ()=>{
         setValue(value+1)
 }
 
-    const onClickHandlerSet = ()=>{
-        localStorage.setItem('curentValue',JSON.stringify(value))
-        localStorage.setItem('curentValue55',JSON.stringify(value+5))
-    }
-
-    const onClickHandlerGEt = ()=>{
-      let GetItem = localStorage.getItem('curentValue')
-        if(GetItem){const newValue =JSON.parse(GetItem)
-        setValue(newValue)}
-
-    }
-
-    const onClickHandlerClear = ()=>{
-      localStorage.clear()
-        setValue(0)
-
-    }
-
-    const onClickHandlerRemove = ()=>{
-        localStorage.removeItem('curentValue55')
 
 
-    }
+
+
 
     return (
         <div className="App">
            <LocalStorage value={value}/>
             <button onClick={onClickHandler}>inc</button>
-            <button onClick={onClickHandlerSet}>setLocalstorage</button>
-            <button onClick={onClickHandlerGEt}>getLocalStorage</button>
-            <button onClick={onClickHandlerClear}>Clear</button>
-            <button onClick={onClickHandlerRemove}>remove</button>
+
+
 
 
 
